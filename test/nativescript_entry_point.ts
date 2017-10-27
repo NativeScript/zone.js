082d92aa0d8de1b4e5c5be717b91f3c588483740
Justin DuJardin <justin@dujardinconsulting.com>
2017-10-27 11:32:36 -0700

    feat(testing): add mocha and jasmine patch outputs for nativescript
     - add gulp build tasks for mocha/jasmine testing files for {N}
     - there are three files in nativescript-angular/zone-js/dist now. The dist file (which is updated to 0.8.18) and a file to include when doing angular testing with jasmine as well as one to include for mocha testing.
     - update mocha patch to be tolerant of envs {N} where there is no global window object.
     - run updated tests and disable ones with platform incompatibilities

--- test/nativescript_entry_point.ts
+++ test/nativescript_entry_point.ts
@@ -1,21 +1,12 @@
 // Must be loaded before zone loads, so that zone can detect WTF.
 import './wtf_mock';
-import './custom_error';
 
 // Setup tests for Zone without microtask support
-import '../lib/zone';
 import '../lib/nativescript/nativescript';
-import '../lib/zone-spec/async-test';
-import '../lib/zone-spec/fake-async-test';
-import '../lib/zone-spec/long-stack-trace';
-import '../lib/zone-spec/proxy';
-import '../lib/zone-spec/sync-test';
-import '../lib/zone-spec/task-tracking';
-import '../lib/zone-spec/wtf';
+import '../lib/nativescript/nativescript.jasmine';
 
 // Setup test environment
-import './test-env-setup';
+import './test-env-setup-jasmine';
 
 // List all tests here:
-import './common_tests';
 import './nativescript_tests';
